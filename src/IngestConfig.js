6dddd7fd5d6f24aa619639e9c63ff900fe46c7df
Thomas Guinto <tjguinto@miners.utep.edu>
2023-04-10 17:38:54 -0600

    Added Ingest Config file button

---
+++ src/IngestConfig.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function IngestConfig() {
+  return (
+    <div className="App">
+      <button>Ingest Config Files</button>
+    </div>
+  );
+}
+
+export default IngestConfig;
